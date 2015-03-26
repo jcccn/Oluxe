eb75d45a0c36ea9c3a1580fe0f7261443d2dcfbe
jcccn <jccuestc@gmail.com>
2015-03-26 19:46:10 +0800

    创建工程

---
+++ Oluxe/Oluxe/Oluxe-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <BlocksKit/BlocksKit+UIKit.h>
+#import <TPKeyboardAvoiding/TPKeyboardAvoidingScrollView.h>
+#import <UMAnalytics/MobClick.h>
+#import <UMFeedback/UMFeedback.h>
+#import <RESideMenu/RESideMenu.h>
+#import <RETableViewManager/RETableViewManager.h>
+#import <Masonry/Masonry.h>
+#import <Appirater/Appirater.h>
---
+++ Oluxe/Oluxe/ViewControllers/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Oluxe
+//
+//  Created by Jiang Chuncheng on 3/26/15.
+//  Copyright (c) 2015 MorganStark. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
